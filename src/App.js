4f1bca7c3c347850854316ff1a8b4c2e5673b523
Student <example@example.com>
2019-09-26 08:39:39 -0700

    It shows how deleteTodo function will work with console.log

--- src/App.js
+++ src/App.js
@@ -9,11 +9,14 @@ class App extends Component {
     ]
   }
 
+  deleteTodo = (id) => {
+    console.log(id)
+  }
   render() {
     return(
-      <div className="App">
+      <div className="todo-app container">
         <h1 className="center blue-text">Todo's</h1>
-        <Todos todos={this.state.todos}/>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
       </div>
     )
   }
