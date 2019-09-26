9e2b986a978b75f7e07ebfd764d48d7a84f5a547
Student <example@example.com>
2019-09-26 08:28:35 -0700

    It makes todo list from hard-coded props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Todos from './Todos';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  state = {
+    todos: [
+      {id: 1, content: "buy some milk"},
+      {id: 2, content: "play mario kart"}
+    ]
+  }
 
-    </div>
-  );
+  render() {
+    return(
+      <div className="App">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos}/>
+      </div>
+    )
+  }
 }
 
 export default App;
